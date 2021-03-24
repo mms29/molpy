2e8676541572ecd228a559b0cb09432c6d029f05
ubuntu-laptop <rvuillemot@gmail.com>
2021-03-24 15:01:27 +0100

    main_p97_all_atoms fix gad biased

--- main_p97.py
+++ main_p97.py
@@ -20,18 +20,18 @@ size=128
 voxel_size=1.458
 threshold=4
 gaussian_sigma=2
-target_density = Volume.from_file('data/P97/emd_3299_128_filtered.mrc', voxel_size=voxel_size, sigma=gaussian_sigma, threshold=threshold)
 init_density = Volume.from_coords(coord=init.coords, size=size, voxel_size=voxel_size, sigma=gaussian_sigma, threshold=threshold)
 
+target_density = Volume.from_file('data/P97/emd_3299_128_filtered.mrc', voxel_size=voxel_size, sigma=gaussian_sigma, threshold=threshold)
 target_density.data = (target_density.data / target_density.data.max())* init_density.data.max()
 # target_density.rescale(init_density, "opt")
 target_density.compare_hist(init_density)
 
-# from src.simulation import nma_deform
-# target = nma_deform(init, [0,0,0,-1500])
-# target_density = Volume.from_coords(coord=target.coords, size=size, voxel_size=voxel_size, sigma=gaussian_sigma, threshold=threshold)
-# chimera_molecule_viewer([init, target])
-# chimera_fit_viewer(mol=init, target=target_density)
+from src.simulation import nma_deform
+target = nma_deform(init, [0,0,0,-1500])
+target_density = Volume.from_coords(coord=target.coords, size=size, voxel_size=voxel_size, sigma=gaussian_sigma, threshold=threshold)
+chimera_molecule_viewer([init, target])
+chimera_fit_viewer(mol=init, target=target_density)
 
 
 params ={
@@ -56,13 +56,13 @@ fitxq  =FlexibleFitting(init=init, target=target_density, vars=["local", "global
 
 fits = multiple_fitting(models=[fitx, fitq, fitxq], n_chain=n_chain, n_proc=24)
 
-fits[0].show(save="results/p97_allatoms_exp_fitx.png")
-fits[1].show(save="results/p97_allatoms_exp_fitq.png")
-fits[2].show(save="results/p97_allatoms_exp_fitxq.png")
+fits[0].show(save="results/p97_allatoms_synth_fitx.png")
+fits[1].show(save="results/p97_allatoms_synth_fitq.png")
+fits[2].show(save="results/p97_allatoms_synth_fitxq.png")
 
-fits[0].res["mol"].save_pdb("results/p97_allatoms_exp_fitx.pdb")
-fits[1].res["mol"].save_pdb("results/p97_allatoms_exp_fitq.pdb")
-fits[2].res["mol"].save_pdb("results/p97_allatoms_exp_fitxq.pdb")
+fits[0].res["mol"].save_pdb("results/p97_allatoms_synth_fitx.pdb")
+fits[1].res["mol"].save_pdb("results/p97_allatoms_synth_fitq.pdb")
+fits[2].res["mol"].save_pdb("results/p97_allatoms_synth_fitxq.pdb")
 
 import matplotlib.pyplot as plt
 from src.functions import cross_correlation
@@ -79,7 +79,7 @@ ax.set_ylabel("Correlation Coefficient")
 ax.set_xlabel("HMC iteration")
 ax.legend(loc="lower right", fontsize=9)
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-fig.savefig("results/p97_allatoms_exp_fits.png")
+fig.savefig("results/p97_allatoms_synth_fits.png")
 
 
 
