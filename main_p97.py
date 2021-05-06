f819e4dfa349d093210542dd0fb36bca615b7811
ubuntu-laptop <rvuillemot@gmail.com>
2021-05-06 16:41:06 +0200

    grad CC

--- main_p97.py
+++ main_p97.py
@@ -44,7 +44,7 @@ target_density.data = (target_density.data / target_density.data.max())* init_de
 target_density.resize(200)
 
 params ={
-    "biasing_factor" : 0.1,
+    "biasing_factor" : 100000,
     "potential_factor" : 1,
     "potentials":["bonds", "angles", "dihedrals", "impropers", "urey", "vdw","elec"],
     "cutoffpl": 6.0,
@@ -63,15 +63,15 @@ params ={
     "target" : target,
     "limit" :100,
     "nb_update":20,
-    "gradient":"LS"
+    "gradient":"CC"
 }
 n_chain=4
 verbose=2
 
 fitx  =FlexibleFitting(init=init, target=target_density, vars=["local"], params=params, n_chain=n_chain, verbose=verbose,
-                       prefix="results/P97_new/fitx_exp_ls")
+                       prefix="results/P97_new/fitx_exp_cc")
 fita  =FlexibleFitting(init=init, target=target_density, vars=["local", "global"], params=params, n_chain=n_chain, verbose=verbose,
-                       prefix="results/P97_new/fita_exp_ls")
+                       prefix="results/P97_new/fita_exp_cc")
 # fita.HMC_chain()
 params["n_step"]=10
 params["n_iter"]=500
